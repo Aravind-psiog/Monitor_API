52c85ffe981336f34734712c157b78bbc7a97007
Aravind-psiog <aravind.prabaharan@psiog.com>
2022-03-15 10:37:48 +0530

    bcrypt fix

--- apivone/managers/hashmanager.py
+++ apivone/managers/hashmanager.py
@@ -1,18 +1,18 @@
 from passlib.context import CryptContext
-import bcrypt
+# import bcrypt
 
-#pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-salt = bcrypt.gensalt()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# salt = bcrypt.gensalt()
 
 def hash_password(password):
-    password = password.encode('utf-8')
-    hashed_password = bcrypt.hashpw(password, salt)
-    #hashed_password = pwd_context.hash(password)
+    # password = password.encode('utf-8')
+    # hashed_password = bcrypt.hashpw(password, salt)
+    hashed_password = pwd_context.hash(password)
     return hashed_password
 
 
 def verify_hash(plain,hashed):
-    plain = plain.encode('utf-8')
-    hashed = hashed.encode('utf-8')
-    return bcrypt.checkpw(plain, hashed)
+    # plain = plain.encode('utf-8')
+    # hashed = hashed.encode('utf-8')
+    # return bcrypt.checkpw(plain, hashed)
     return pwd_context.verify(plain,hashed)
